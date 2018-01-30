6d264d73acc1b9b432fdd14bcd07f5f00b89bce4
Alex Sartan <asartan@pivotal.io>
2018-01-30 16:51:40 -0700

    add database to app

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -36,8 +37,9 @@ namespace PalTracker
                 Configuration.GetValue<string>("CF_INSTANCE_ADDR", "")
             ));
 
-            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
 
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
